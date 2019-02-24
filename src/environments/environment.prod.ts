4b19aa59c7f02145da9dde62b63873e57bbac955
Raniel Gomes da Silva <raniel90@gmail.com>
2019-02-24 14:34:36 -0300

    PWA installable configs to iOS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyASZ5Z2-7RjSLVTeCZwROqY9vnK3_DOTso",
+  authDomain: "church-music-manager.firebaseapp.com",
+  databaseURL: "https://church-music-manager.firebaseio.com",
+  projectId: "church-music-manager",
+  storageBucket: "church-music-manager.appspot.com",
+  messagingSenderId: "1037354223731"
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyASZ5Z2-7RjSLVTeCZwROqY9vnK3_DOTso",
+  authDomain: "church-music-manager.firebaseapp.com",
+  databaseURL: "https://church-music-manager.firebaseio.com",
+  projectId: "church-music-manager",
+  storageBucket: "church-music-manager.appspot.com",
+  messagingSenderId: "1037354223731"
 };
 
 /*
